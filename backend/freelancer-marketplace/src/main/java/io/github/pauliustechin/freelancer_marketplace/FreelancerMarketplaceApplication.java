7f3514163111ef88adb061f690f53177ca0da20d
pauliustechin <pauliustechin@gmail.com>
2026-04-29 19:19:16 +0300

    add, setup and test backend project

---
+++ backend/freelancer-marketplace/src/main/java/io/github/pauliustechin/freelancer_marketplace/FreelancerMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package io.github.pauliustechin.freelancer_marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelancerMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelancerMarketplaceApplication.class, args);
+	}
+
+}
---
+++ backend/freelancer-marketplace/src/test/java/io/github/pauliustechin/freelancer_marketplace/FreelancerMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pauliustechin.freelancer_marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelancerMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
